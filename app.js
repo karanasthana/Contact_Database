cf22109bb855781f343f772fa3ef3dea868e9d85
Karan Asthana <karan.asthana1@gmail.com>
2018-02-16 20:50:48 +0530

    Add and Save Another Button added, alongwith Modal functionality to display name!

--- app.js
+++ app.js
@@ -13,6 +13,32 @@
   console.log(typeof name);
 
   saveButton = document.querySelector("#submit_id");
+  saveAndAddButton = document.querySelector("#submit_another_id");
+
+  saveAndAddButton.addEventListener("click",function(){
+  var nameToSave = name_id.value;
+  console.log("logging in to console after getting name as : ",nameToSave); 
+  var emailToSave = email_id.value;
+  var ageToSave = age_id.value;
+  var stateToSave = state_id.value;
+  var addressToSave = address_id.value;
+  var timeToSave = new Date();
+  console.log("I'll be saving "+nameToSave+", "+emailToSave+", "+ageToSave+", "+stateToSave+", "+addressToSave+" to the Firestore");
+  var data_entry = "samples/"+nameToSave+"-"+emailToSave;
+  const docRef = firestore.doc(data_entry)
+  docRef.set({
+  name:nameToSave,
+  email:emailToSave,
+  age:ageToSave,
+  state:stateToSave,
+  address:addressToSave,
+  date:timeToSave
+  }).then(function() {var fr = document.getElementById("frm");
+  fr.reset();
+  console.log("Contact saved!");
+});
+});
+
   saveButton.addEventListener("click",function(){
   var nameToSave = name_id.value;
   console.log("logging in to console after getting name as : ",nameToSave); 
@@ -32,10 +58,8 @@
   address:addressToSave,
   date:timeToSave
   }).then(function() {
-  console.log("Contact saved!");
+    location.href = 'second_page.html';
   }).catch(function (error) {
   console.log("Error! :",error);
-  }).then(function() {
-    window.location.href = 'second_page.html';
   });
-  })
+  });
